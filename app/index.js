c0e713e7eef30be2c0e225df17d6a32d82f1a908
RenaudF <sirgzu@hotmail.com>
2020-11-08 10:27:02 +0100

    extracting values from graph labels
    introduced graphlib-dot

--- app/index.js
+++ app/index.js
@@ -1,1 +1,19 @@
-d3.text("test.gv").then(console.log);
+d3.text("test.gv")
+  .then(graphlibDot.read)
+  .then(preprocess)
+  .then(graphlibDot.write)
+  .then(console.log);
+
+function preprocess(graph) {
+  graph.nodes().forEach((id) => {
+    const node = graph.node(id);
+    const [, value] = node.label.split("\\n").map((s) => +s.trim());
+    graph.setNode(id, { ...node, value });
+  });
+  graph.edges().forEach(({ v, w }) => {
+    const edge = graph.edge(v, w);
+    const value = +edge.label;
+    graph.setEdge(v, w, { ...edge, value });
+  });
+  return graph;
+}
