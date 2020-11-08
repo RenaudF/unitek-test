0345b79ed932fe3008c78c620caaa8bc905fe9b6
RenaudF <sirgzu@hotmail.com>
2020-11-08 15:38:20 +0100

    now using modules

---
+++ app/utils.js
@@ -0,0 +1,13 @@
+export function preprocess(graph) {
+  graph.nodes().forEach((id) => {
+    const node = graph.node(id);
+    const [, value] = node.label.split("\\n").map((s) => +s.trim());
+    graph.setNode(id, { ...node, value });
+  });
+  graph.edges().forEach(({ v, w }) => {
+    const edge = graph.edge(v, w);
+    const value = +edge.label;
+    graph.setEdge(v, w, { ...edge, value });
+  });
+  return graph;
+}
