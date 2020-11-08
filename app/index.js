208d8847c18576296dbc04a8783c75081d476528
RenaudF <sirgzu@hotmail.com>
2020-11-08 10:30:12 +0100

    added visual dimensions

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
-import { preprocess } from "./utils.js";
+import { preprocess, process } from "./utils.js";
 
 d3.text("test.gv")
   .then(graphlibDot.read)
   .then(preprocess)
+  .then(process)
   .then(graphlibDot.write)
   .then((dotString) => d3.select("#graph").graphviz().renderDot(dotString));
--- app/utils.js
+++ app/utils.js
@@ -1,13 +1,61 @@
+function getNodes(graph) {
+  return graph.nodes().map((id) => ({ id, node: graph.node(id) }));
+}
+
+function getEdges(graph) {
+  return graph.edges().map(({ v, w }) => ({ v, w, edge: graph.edge(v, w) }));
+}
+
+/** Extract values from nodes and edges labels */
 export function preprocess(graph) {
-  graph.nodes().forEach((id) => {
-    const node = graph.node(id);
+  getNodes(graph).forEach(({ id, node }) => {
     const [, value] = node.label.split("\\n").map((s) => +s.trim());
     graph.setNode(id, { ...node, value });
   });
-  graph.edges().forEach(({ v, w }) => {
-    const edge = graph.edge(v, w);
+  getEdges(graph).forEach(({ v, w, edge }) => {
     const value = +edge.label;
     graph.setEdge(v, w, { ...edge, value });
   });
+  return graph;
+}
+
+/** Adds visual dimensions to nodes and edges */
+export function process(graph) {
+  // setting all nodes color gradient
+  const nodes = getNodes(graph);
+  const nodeValueExtent = d3.extent(nodes, ({ node: { value } }) => value);
+  const nodeColourScale = d3
+    .scaleLinear()
+    .domain(nodeValueExtent)
+    .range(["blue", "orange"]);
+
+  nodes.forEach(({ id, node }) => {
+    const fillcolor = d3.color(nodeColourScale(node.value)).formatHex();
+    graph.setNode(id, { ...node, fillcolor, style: "filled" });
+  });
+
+  // setting all edges thickness
+  const edges = getEdges(graph);
+  const edgeValueExtent = d3.extent(edges, ({ edge: { value } }) => value);
+  const edgeThicknessScale = d3
+    .scaleSqrt()
+    .domain(edgeValueExtent)
+    .range([1, 5]);
+
+  edges.forEach(({ v, w, edge }) => {
+    const penwidth = edgeThicknessScale(edge.value);
+    graph.setEdge(v, w, { ...edge, penwidth });
+  });
+
+  // setting top value edge color and related node shapes
+  const edgesTopValue = d3.max(edges, ({ edge: { value } }) => value);
+  const { v, w } = edges.find(({ edge: { value } }) => value === edgesTopValue);
+  const shape = "box";
+  const color = "green";
+
+  graph.setNode(v, { ...graph.node(v), shape });
+  graph.setNode(w, { ...graph.node(w), shape });
+  graph.setEdge(v, w, { ...graph.edge(v, w), color });
+
   return graph;
 }
