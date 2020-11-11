61d5e6c1c37677107723979544ecf2e8b62ee5be
RenaudF <sirgzu@hotmail.com>
2020-11-11 08:30:22 +0100

    added d3-force zoom

--- app/d3-force.js
+++ app/d3-force.js
@@ -238,7 +238,7 @@ export function render({ nodes, edges }) {
       }) => label
     );
 
-  simulation.on("tick", () => {
+  function updateEdgeLabelNodes() {
     edgeLabelNodes.forEach((edgeLabelNode) => {
       const { source, label, edge, circular } = edgeLabelNode;
       source.fx = source.x = edge.source.x;
@@ -262,6 +262,10 @@ export function render({ nodes, edges }) {
         middle.y = middle.fy = _middle.y;
       }
     });
+  }
+
+  simulation.on("tick", () => {
+    updateEdgeLabelNodes();
 
     labelNodeSimulations.forEach((labelSimulation) =>
       labelSimulation.alpha(simulation.alpha()).restart()
@@ -293,6 +297,46 @@ export function render({ nodes, edges }) {
     inner.attr("transform", d3.zoomTransform(inner.node()));
   });
   svg.call(zoom);
+
+  // zoom features
+  const resetTranslate = () => {
+    const svgBCR = svg.node().getBoundingClientRect();
+    const gBCR = inner.node().getBoundingClientRect();
+    const dx = svgBCR.left + svgBCR.width / 2 - gBCR.left - gBCR.width / 2;
+    const dy = svgBCR.top + svgBCR.height / 2 - gBCR.top - gBCR.height / 2;
+    const transform = inner.attr("transform");
+    const [, scale] = /scale\((.*?)\)/.exec(transform);
+    svg.call(
+      zoom.transform,
+      d3.zoomTransform(inner.node()).translate(dx / scale, dy / scale)
+    );
+  };
+
+  const resetScale = () => {
+    const svgBCR = svg.node().getBoundingClientRect();
+    const gBCR = inner.node().getBoundingClientRect();
+    const heightRatio = svgBCR.height / gBCR.height;
+    const widthRatio = svgBCR.width / gBCR.width;
+    const scaleFactor = 0.95; // to leave some margins on the sides
+    const scale = Math.min(heightRatio, widthRatio) * scaleFactor;
+    svg.call(zoom.transform, d3.zoomTransform(inner.node()).scale(scale));
+  };
+
+  // dry run the simulation before rendering and initiating zoom to have better coordinates
+  d3.range(50).forEach(() => {
+    updateEdgeLabelNodes();
+    simulation.tick();
+    labelNodeSimulations.forEach((labelNodeSimulation) =>
+      labelNodeSimulation.tick()
+    );
+  });
+  requestAnimationFrame(() => {
+    resetScale();
+    resetTranslate();
+  });
+
+  // event handlers
+  addEventListener("resize", resetScale);
 
   registerHandlers();
 }
